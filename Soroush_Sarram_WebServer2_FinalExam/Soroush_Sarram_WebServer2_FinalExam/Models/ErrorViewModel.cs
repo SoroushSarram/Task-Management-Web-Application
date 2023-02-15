0ea4be482db9fdc208f26483d5471e1096369167
Soroush Sarram <sarram.sor@gmail.com>
2023-02-14 20:47:58 -0500

    deleted

--- Soroush_Sarram_WebServer2_FinalExam/Soroush_Sarram_WebServer2_FinalExam/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Soroush_Sarram_WebServer2_FinalExam.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
