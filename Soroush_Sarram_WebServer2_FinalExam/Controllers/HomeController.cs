14cc3a0e8b1cf452728c4d154c62bc6bc66265b9
Soroush Sarram <sarram.sor@gmail.com>
2023-02-14 20:49:05 -0500

    Files upload

---
+++ Soroush_Sarram_WebServer2_FinalExam/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Soroush_Sarram_WebServer2_FinalExam.Models;
+using System.Diagnostics;
+
+namespace Soroush_Sarram_WebServer2_FinalExam.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Soroush_Sarram_WebServer2_FinalExam/Data/WebServer2FinalExamContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Soroush_Sarram_WebServer2_FinalExam.Models;
+
+namespace Soroush_Sarram_WebServer2_FinalExam.Data
+{
+    public partial class WebServer2FinalExamContext : DbContext
+    {
+        public WebServer2FinalExamContext()
+        {
+        }
+
+        public WebServer2FinalExamContext(DbContextOptions<WebServer2FinalExamContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Models.Task> Tasks { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+                optionsBuilder.UseSqlServer("Data Source=SOROUSH\\SQLEXPRESS; Initial catalog=WebServer2FinalExam; Trusted_Connection=True; TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.UserId).ValueGeneratedNever();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
